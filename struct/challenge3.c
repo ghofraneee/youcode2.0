7bc5d3e8d6d9ca261a4dd7342473a2fd96e02716
“[GHOFRANE <[Chemnaneg@gmail.com]>
2025-09-25 18:52:10 +0100

    ;

--- struct/challenge3.c
+++ struct/challenge3.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include<string.h>
 
-
-struct rrecta{
+struct rrecta {
     float longe;
     float larg;
 };
-
 
 float calcul(struct rrecta r) {
     return r.longe * r.larg;
 }
 
 int main() {
-    struct rrecta rect = {6.22, 2.0};
+    struct rrecta rect;
+    rect.longe = 6.22;
+    rect.larg = 2.0;
+
     float aire = calcul(rect);
 
-    printf("aire du rect: %f\n", aire);
+    printf("aire de rect : %f\n", aire);
 
     return 0;
 }
+
