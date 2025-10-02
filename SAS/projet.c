37a9626070e9bb8af26e1459015a2277c5e8c0f8
“[GHOFRANE <[Chemnaneg@gmail.com]>
2025-10-02 15:08:11 +0100

    ;

--- SAS/projet.c
+++ SAS/projet.c
@@ -15,6 +15,8 @@ typedef struct
     float poids;
 }Animal;
 Animal A[M];
+char zoohabitat[4][10] = {"Savane", "Jungle", "Desert", "Aquatique"};
+char habitatCherche[M];
 int taille=0;
 int dernierID =1;
 int newid;
@@ -23,6 +25,7 @@ int choix2;
 
 int menu();
 void ajouter();
+
 void ajouterunique();
 void ajouterplus();
 void afficher();
@@ -37,11 +40,18 @@ void modifierhabitat();
 void modifierage();
 void supprimer();
 void rechercher();
+void rechercherparid();
+void rechercherparnom();
+void rechercherparespece();
 void statistiques();
+void nbrtotal();
+void agemoyen();
+void plusjeune();
+void statespace();
 
 void initZoo() {
     Animal exemple[20] = {
-        {1, "Leo",      "Lion",       5,  "Savane",    190.5},
+     {1, "Leo",      "Lion",       5,  "Savane",    190.5},
         {2, "Mimi",     "Singe",      3,  "Jungle",    35.2},
         {3, "Ella",     "Elephant",  10,  "Savane",   5400.0},
         {4, "Koko",     "Gorille",    7,  "Jungle",   160.7},
@@ -50,17 +60,17 @@ void initZoo() {
         {7, "Pipo",     "Perroquet",  2,  "Jungle",    0.9},
         {8, "Toto",     "Tigre",      6,  "Jungle",   220.3},
         {9, "Nemo",     "Poisson",    1,  "Aquatique", 0.5},
-        {10, "Manny",   "Mammouth",  25,  "Toundra", 6000.0},
+        {10, "Manny",   "Mammouth",  25,  "Savane", 6000.0},
         {11, "Lili",    "Lezard",     3,  "Desert",    1.1},
-        {12, "Oscar",   "Ours",       8,  "Foret",    300.0},
+        {12, "Oscar",   "Ours",       8,  "Savane",    300.0},
         {13, "Sasha",   "Serpent",    4,  "Jungle",    3.2},
-        {14, "Max",     "Loup",       5,  "Foret",     45.7},
-        {15, "Bella",   "Chevre",     2,  "Montagne",  25.0},
+        {14, "Max",     "Loup",       5,  "Savane",     45.7},
+        {15, "Bella",   "Chevre",     2,  "Desert",  25.0},
         {16, "Charly",  "Chameau",    7,  "Desert",   400.0},
         {17, "Daisy",   "Dauphin",   12,  "Aquatique", 200.0},
-        {18, "Rex",     "Chien",      6,  "Maison",    20.0},
-        {19, "Coco",    "Crocodile",  9,  "Marais",   500.0},
-        {20, "Momo",    "Mouette",    3,  "Plage",     1.5}
+        {18, "Rex",     "Chien",      6,  "Maison",    20.0},   
+        {19, "Coco",    "Crocodile",  9,  "Aquatique",   500.0},
+        {20, "Momo",    "Mouette",    3,  "Savane",     1.5}
     };
 
     for (int i = 0; i < 20; i++) {
@@ -99,13 +109,14 @@ int main(){
            modifier();
            break;
         case 4:
+          supprimer();
            break;
         case 5:
            rechercher();
            break;
-        case 6:
+        /*case 6:
             statistiques();
-            break;
+            break;*/
         case 7:
              printf("Au revoir\n");
             break;
@@ -142,6 +153,15 @@ void ajouter() {
         default: printf("choix invalide.\n");
     }
 }
+int habitatvalide(char habitat[]) {
+    for (int i = 0; i < 4; i++) {
+        if (strcmpi(habitat, zoohabitat[i]) == 0) {
+            return 1; 
+        }
+    }
+    return 0;
+}
+
 void ajouterunique() {
     if (taille >= M) {
         printf("Le zoo est plein.\n");
@@ -162,8 +182,16 @@ void ajouterunique() {
     printf("entrez l'age de animal:\n");
     scanf("%d", &A[taille].age);
 
-    printf("entrez habitat:\n");
-    scanf("%s", A[taille].habitat);
+    while (1) {
+        printf("entrez habitat (Savane, Jungle, Desert, Aquatique) :\n");
+        scanf("%s", A[taille].habitat);
+
+        if (habitatvalide(A[taille].habitat)) {
+            break;
+        } else {
+            printf("habitat invalide ! choisir parmi : Savane, Jungle, Desert, Aquatique\n");
+        }
+    }
 
     printf("entrez le poids de animal:\n");
     scanf("%f", &A[taille].poids);
@@ -213,9 +241,9 @@ void afficher() {
     case 3:
     afficherage();
     break;
-   /*case 4:
+   case 4:
     afficherhabitat();
-    break;*/
+    break;
     
     default:printf("Choix invalide\n");
         break;
@@ -299,9 +327,27 @@ void afficheragedecroi(){
     printf("\nListe est trie par age \n");
     afficherlist();
 }
-/*void afficherhabitat(){
+void afficherhabitat(){
+    char habitatrecherche[M];
+    int trouve = 0;
 
-}*/
+    printf("entrez l'habitat à afficher (Savane, Jungle, Desert, Aquatique) : ");
+    scanf("%s", habitatrecherche);
+
+    printf("\nAnimaux vivant dans l'habitat '%s' :\n", habitatrecherche);
+
+    for (int i = 0; i < taille; i++) {
+        if (strcasecmp(A[i].habitat, habitatrecherche) == 0) {
+            printf("ID: %d, Nom: %s, Espèce: %s, Age: %d, Poids: %.2f\n",
+                A[i].id, A[i].nom, A[i].espece, A[i].age, A[i].poids);
+            trouve = 1;
+        }
+    }
+
+    if (!trouve) {
+        printf("Aucun animal trouvé dans cet habitat.\n");
+    }
+}
 
 void modifier(){
     int choix5;
@@ -312,9 +358,9 @@ void modifier(){
     scanf("%d", &choix5);
     switch (choix5)
     {
-    /*case 1:
+    case 1:
      modifierhabitat();
-    break;*/
+    break;
     case 2:
     modifierage();
     break;
@@ -324,7 +370,41 @@ void modifier(){
     }
 
 }
-/*void modifierhabitat(){}*/
+void modifierhabitat(){
+
+    int idAnimal;
+    char nouveauHabitat[M];
+    int trouve = 0;
+
+    printf("Entrez l'ID de l'animal dont vous voulez modifier l'habitat : ");
+    scanf("%d", &idAnimal);
+
+    for (int i = 0; i < taille; i++) {
+        if (A[i].id == idAnimal) {
+            trouve = 1;
+
+            while (1) {
+                printf("Entrez le nouvel habitat (Savane, Jungle, Desert, Aquatique) : ");
+                scanf("%s", nouveauHabitat);
+
+                if (habitatvalide(nouveauHabitat)) {
+                    strcpy(A[i].habitat, nouveauHabitat);
+                    printf("Habitat modifié avec succès pour l'animal ID %d\n", idAnimal);
+                    break;
+                } else {
+                    printf("Habitat invalide .\nchoisir parmi : Savane, Jungle, Desert, Aquatique\n");
+                }
+            }
+            break;
+        }
+    }
+
+    if (!trouve) {
+        printf("Aucun animal trouvé avec cet ID.\n");
+    
+}
+
+}
 void modifierage() {
     char nom[M];
     int nouvage;
@@ -336,11 +416,11 @@ void modifierage() {
     for (int i = 0; i < taille; i++) {
         if (strcmpi(A[i].nom, nom) == 0) {
             A[i].age = nouvage;
-            printf("Âge modifié avec succès pour %s.\n", A[i].nom);
+            printf("Âge modifié avec succès pour %s\n", A[i].nom);
             return;
         }
     }
-    printf("Animal non trouvé.\n");
+    printf("Animal non trouvé\n");
 }
 void supprimer() {
     if (taille == 0) {
@@ -365,13 +445,140 @@ void supprimer() {
             break;
         }
     }
-
     if (!trouve) {
         printf("Aucun animal trouvé avec cet ID\n");
     }
 }
+void rechercher() {
+    int choix6;
 
-     
+    if (taille >= M) {
+        printf("Le zoo est plein !\n");
+        return;
+    }
+
+    printf("\n===== RECHERCHER UN ANIMAL =====\n");
+    printf("1 - Rechercher un animal par ID\n");
+    printf("2 - Rechercher un animal par NOM\n");
+    printf("3 - Rechercher un animal par ESPECE\n");
+
+    printf("Entrez votre choix : ");
+    scanf("%d", &choix6);
+
+    switch (choix6) {
+        case 1: 
+        rechercherparid();
+         break;
+        case 2: 
+        rechercherparnom();
+         break;
+         case 3 :
+         rechercherparespece();
+        default: printf("choix invalide.\n");
+    }
+}
+void rechercherparid(){
+    int id;
+    int trouve = 0;
+
+    printf("Entrez l'ID de l'animal à rechercher : ");
+    scanf("%d", &id);
+
+    for (int i = 0; i < taille; i++) {
+        if (A[i].id == id) {
+            printf("\nAnimal trouvé :\n");
+            printf("ID       : %d\n", A[i].id);
+            printf("Nom      : %s\n", A[i].nom);
+            printf("Espèce   : %s\n", A[i].espece);
+            printf("Âge      : %d\n", A[i].age);
+            printf("Habitat  : %s\n", A[i].habitat);
+            printf("Poids    : %.2f\n", A[i].poids);
+            trouve = 1;
+            break;
+        }
+    }
+
+    if (!trouve) {
+        printf("Aucun animal trouvé avec l'ID %d.\n", id);
+    }
+}
 
 
+
+void rechercherparnom(){
+    char nom[M];
+    int trouve = 0;
+
+    printf("Entrez le nom de l'animal à rechercher : ");
+    scanf("%s", nom);
+
+    for (int i = 0; i < taille; i++) {
+        if (strcmpi(A[i].nom, nom) == 0) {
+            printf("\nAnimal trouvé :\n");
+            printf("ID       : %d\n", A[i].id);
+            printf("Nom      : %s\n", A[i].nom);
+            printf("Espèce   : %s\n", A[i].espece);
+            printf("Âge      : %d\n", A[i].age);
+            printf("Habitat  : %s\n", A[i].habitat);
+            printf("Poids    : %.2f\n", A[i].poids);
+            trouve = 1;
+        }
+    }
+
+    if (!trouve) {
+        printf("Aucun animal trouvé avec le nom \"%s\".\n", nom);
+    }
+
+
+
+}
+void rechercherparespece(){
+
+    char espece[M];
+    int trouve = 0;
+
+    printf("Entrez l'espèce de l'animal à rechercher : ");
+    scanf("%s", espece);
+
+    printf("\nRésultats pour l'espèce \"%s\" :\n", espece);
+    for (int i = 0; i < taille; i++) {
+        if (strcmpi(A[i].espece, espece) == 0) {
+            printf("ID       : %d\n", A[i].id);
+            printf("Nom      : %s\n", A[i].nom);
+            printf("Âge      : %d\n", A[i].age);
+            printf("Habitat  : %s\n", A[i].habitat);
+            printf("Poids    : %.2f\n\n", A[i].poids);
+            trouve = 1;
+        }
+    }
+
+    if (!trouve) {
+        printf("Aucun animal trouvé avec l'espèce \"%s\".\n", espece);
+    }
+}
+void statistique(){
+        int choix7;
+
+    printf("\n===== STATISTIQUES =====\n");
+    printf("1 - Nombre total d'animaux dans le zoo\n");
+    printf("2 - Age moyen des animaux\n");
+    printf("3 - Plus vieux et plus jeune animal\n");
+    printf("3 - Afficher les espèces les plus représentées.\n");
+
+
+    printf("Entrez votre choix : ");
+    scanf("%d", &choix7);
+
+    switch (choix7) {
+        case 1: 
+        rechercherparid();
+         break;
+        case 2: 
+        rechercherparnom();
+         break;
+         case 3 :
+         rechercherparespece();
+        default: printf("choix invalide.\n");
+    }
+}
 
